da66ace99dff3e7cffeb07c6fd313d0515b677ea
The Full-Stack Jonin <francis94c@gmail.com>
2024-10-01 16:36:45 +0100

    Refactoring.

--- routes/api.dart
+++ routes/api.dart
@@ -1,5 +1,6 @@
+import 'package:daravel_core/daravel_core.dart';
+
 import '../app/http/controllers/landing_controller.dart';
-import '../core/http/daravel_router.dart';
 
 final apiRouter = DaravelRouter();
 
